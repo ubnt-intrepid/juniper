a42364ee91d7d117a041e6790d8c99eb77284589
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2018-09-14 17:55:59 +0900

    the feature `proc-macro` has been stabilized since 1.29

--- juniper_codegen/src/lib.rs
+++ juniper_codegen/src/lib.rs
@@ -4,7 +4,6 @@
 //! You should not depend on juniper_codegen directly.
 //! You only need the `juniper` crate.
 
-#![cfg_attr(feature = "nightly", feature(proc_macro))]
 #![recursion_limit = "1024"]
 
 extern crate proc_macro;
